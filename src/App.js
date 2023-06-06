e787da02ecf841adc8dd0c2e0b8ed8fddf4c5922
kayjee <Kagiso.killagram@gmail.com>
2023-06-06 16:54:53 +0200

    fixed css to match wireframe

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import './components/Calculator.css'; // Import the CSS file
+import Calculator from './components/Calculator'; // Import the Calculator component
 
-const Calculator = () => (
-  <div className="App">
-    <div className="top" />
-    <div className="display">0</div>
-    <div className="buttons">
-      <div className="Button function">AC</div>
-      <div className="Button function">±</div>
-      <div className="Button function">%</div>
-      <div className="Button operator">÷</div>
-      <div className="Button">7</div>
-      <div className="Button">8</div>
-      <div className="Button">9</div>
-      <div className="Button operator">x</div>
-      <div className="Button">4</div>
-      <div className="Button">5</div>
-      <div className="Button">6</div>
-      <div className="Button operator">-</div>
-      <div className="Button">1</div>
-      <div className="Button">2</div>
-      <div className="Button">3</div>
-      <div className="Button operator">+</div>
-      <div className="Button zero">0</div>
-      <div className="Button">.</div>
-      <div className="Button operator">=</div>
-    </div>
-  </div>
-);
+function App() {
+  return (
 
-export default Calculator;
+    <Calculator />
+
+  );
+}
+
+export default App;
